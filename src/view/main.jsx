4af890aacda43bbfe20e8a46bba9de93bf64ae6a
Sunny <sconsuegra@prontobpo.com>
2024-09-02 19:45:02 -0600

    #add import internacionalizacion

--- src/view/main.jsx
+++ src/view/main.jsx
@@ -1,11 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-
 import './index.css'
+
+// Importar internacionalizzcion
+import { ConfigProvider } from 'antd';
+import esEs from 'antd/locale/es_ES';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <ConfigProvider locale={esEs}>
     <App />
+    </ConfigProvider>
   </StrictMode>,
 )
