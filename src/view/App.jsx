e636239880ccf2eca58d9cb480ef5fcf9ef0be74
Sunny <sconsuegra@prontobpo.com>
2024-09-04 15:09:35 -0600

    #add se agrego un comando para grid en appjsx

--- src/view/App.jsx
+++ src/view/App.jsx
@@ -4,17 +4,45 @@ import Prueba from "./Prueba"
 import Calendario from "./Calendario"
 import Valoracion from "./Valoracion"
 import Usuario from "./Usuario"
+import { Col, Row } from 'antd';
 
 // Funciones Principales
 
 function App() {
   return (
     <>
-      <Usuario nombreUsuario={"SunnyTheLoser"}/>
+      <Usuario nombreUsuario={"Sunny"} />
+      <Usuario nombreUsuario={"JuanJo"} apellidoUsuario={"Monzon"}/>
+      <Usuario nombreUsuario={"Esteban"} />
+      <Usuario nombreUsuario={"Pedrito"} apellidoUsuario={"Mejia"}/>
+      <Usuario nombreUsuario={"Kendra"} />
       <Prueba />
       <Calendario />
       <Valoracion />
-    </>
+      <br></br>
+      <Valoracion />
+      <br></br>
+      <Valoracion />
+
+<Row>
+  <Col span={24}>col</Col>
+</Row>
+<Row>
+  <Col span={12}>col-12</Col>
+  <Col span={12}>col-12</Col>
+</Row>
+<Row>
+  <Col span={8}>col-8</Col>
+  <Col span={8}>col-8</Col>
+  <Col span={8}>col-8</Col>
+</Row>
+<Row>
+  <Col span={6}>col-6</Col>
+  <Col span={6}>col-6</Col>
+  <Col span={6}>col-6</Col>
+  <Col span={6}>col-6</Col>
+</Row>
+</>
   )
 }
 
