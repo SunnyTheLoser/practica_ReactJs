b6d904c83f316486c991a0e79e00e4ca482ba59d
Sunny <sconsuegra@prontobpo.com>
2024-09-13 13:35:02 -0600

    #add changes

--- src/view/App.jsx
+++ src/view/App.jsx
@@ -5,6 +5,9 @@ import Calendario from "./Calendario"
 import Valoracion from "./Valoracion"
 import Usuario from "./Usuario"
 import { Col, Row } from 'antd';
+import { purple, } from '@ant-design/colors';
+
+
 
 // Funciones Principales
 
@@ -22,28 +25,34 @@ function App() {
       <br></br>
       <Valoracion />
       <br></br>
+      <br></br>
+      <Valoracion />
       <Valoracion />
 
-<Row>
+<Row style={{ backgroundColor: purple[3] }}>
   <Col span={24}>col</Col>
 </Row>
 <Row>
-  <Col span={12}>col-12</Col>
-  <Col span={12}>col-12</Col>
+  <Col span={12} style={{ backgroundColor: purple[4] }}>col-12</Col>
+  <Col span={12} style={{ backgroundColor: purple[5] }}>col-12</Col>
 </Row>
 <Row>
-  <Col span={8}>col-8</Col>
-  <Col span={8}>col-8</Col>
-  <Col span={8}>col-8</Col>
+  <Col span={8} style={{ backgroundColor: purple[6] }}>col-8</Col>
+  <Col span={8} style={{ backgroundColor: purple[7] }}>col-8</Col>
+  <Col span={8} style={{ backgroundColor: purple[8] }}>col-8</Col>
 </Row>
 <Row>
-  <Col span={6}>col-6</Col>
-  <Col span={6}>col-6</Col>
-  <Col span={6}>col-6</Col>
-  <Col span={6}>col-6</Col>
+  <Col span={6} style={{ backgroundColor: purple[9] }}>col-6</Col>
+  <Col span={6} style={{ backgroundColor: purple[8] }}>col-6</Col>
+  <Col span={6} style={{ backgroundColor: purple[7] }}>col-6</Col>
+  <Col span={6} style={{ backgroundColor: purple[6] }}>col-6</Col>
 </Row>
+<br></br>
+<br></br>
 </>
+
   )
+
 }
 
 // Exportaciones
